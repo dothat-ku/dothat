a670ee8c53495297388d3f46b19075d4817c221b
dothat-ku <57694591+dothat-ku@users.noreply.github.com>
2019-11-28 15:20:38 +0900

    Update load.php

--- dothat/load.php
+++ dothat/load.php
@@ -18,7 +18,7 @@ if ($_SESSION['banned'] == 'banned_user') {
 // definition of DB connection
 define('DB_HOST', 'localhost');
 define('DB_PORT', '5432');
-define('DB_PASSWD', '9891');
+define('DB_PASSWD', 'your_db_password');
 define('DB_USER', 'postgres');
 define('DB_NAME', 'dothat');
 
